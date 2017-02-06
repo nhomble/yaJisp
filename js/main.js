9c3808e178499f1e49f9cdbb03fc7691f6625098
nicolas <nhomble@terpmail.umd.edu>
2017-02-05 21:35:05 -0500

    congrats hombln, you implemented first/second/last, what a crap interpreter

--- js/main.js
+++ js/main.js
@@ -1,22 +1,65 @@
-(function(){
+(function () {
     // here so I type less
     var TOKEN_TYPE = {
-            SYMBOL: "symbol",
-            VAR:    "variable",
-            FUNC:   "function"
+        SYMBOL: "symbol",
+        VAR: "variable",
+        FUNC: "function"
     };
-    function isStartOfExpression (token){ return token == '('; }
-    function isEndOfExpression(token){ return  token == ')'; }
-    function isWhiteSpace(token){ return token.length == 0; }
-    function isFunctionCall(l){ return l[0].type == TOKEN_TYPE.FUNC; }
-    function clojure(scope, parent){
+    // shouldn't have to repeat get
+    var STD_LIB = {
+        scope: {
+            first: function (args) {
+                return args[0];
+            },
+            second: function (args) {
+                return (args.length > 1) ? args[1] : null;
+            },
+            last: function (args) {
+                return args.slice(-1)[0];
+            }
+        },
+        get: function (variable) {
+            if (variable in this.scope)
+                return this.scope[variable];
+            else if (this.parent !== undefined)
+                return this.parent.get(variable);
+            else
+                console.log(variable + " is undefined!"); // we should handle errors better one day
+        }
+    };
+
+    function isStartOfExpression(token) {
+        return token == '(';
+    }
+
+    function isEndOfExpression(token) {
+        return token == ')';
+    }
+
+    function isWhiteSpace(token) {
+        return token.length == 0;
+    }
+
+    function isFunctionCall(l) {
+        return l[0].type == TOKEN_TYPE.FUNC;
+    }
+
+    function isNumeric(n) {
+        return !isNaN(parseFloat(n));
+    }
+
+    function isString(s) {
+        return s[0] === '"' && s.slice(-1) === '"';
+    }
+
+    function clojure(scope, parent) {
         return {
             scope: scope,
             parent: parent,
-            get: function(variable){
-                if(variable in this.scope)
+            get: function (variable) {
+                if (variable in this.scope)
                     return this.scope[variable];
-                else if(this.parent !== undefined)
+                else if (this.parent !== undefined)
                     return this.parent.get(variable);
                 else
                     console.log(variable + " is undefined!"); // we should handle errors better one day
@@ -24,103 +67,109 @@
         };
     }
 
-    function newClojure(parent){
+    function newClojure(parent) {
         return clojure({}, parent);
     }
 
-    function identify(token){
+    function parse(tokens) {
+        // .. Mary Rose Cook.. we can change this later
+        astHelper = function (tokens, accum) {
+            if (tokens.length == 0)
+                return accum.pop();
+
+            var token = tokens.shift();
+            if (isStartOfExpression(token)) {
+                var childAst = astHelper(tokens, []);
+                accum.push(childAst);
+                return astHelper(tokens, accum);
+            }
+            else if (isEndOfExpression(token)) {
+                return accum;
+            }
+            else if (isWhiteSpace(token)) {
+                return astHelper(tokens, accum);
+            }
+            else {
+                var node = identify(token);
+                accum.push(node);
+                return astHelper(tokens, accum);
+            }
+        };
+
+        return astHelper(tokens, []);
+    }
+
+    // a glorious regex to split things up
+    function tokenize(line) {
+        return line
+            .replace(/\(/g, ' ( ')
+            .replace(/\)/g, ' ) ')
+            .split(/ +/);
+    }
+
+    // we only need to discern vars from symbols, funcs are determined based on the clojure
+    function identify(token) {
+        var token_type;
+        if (isNumeric(token) || isString(token))
+            token_type = TOKEN_TYPE.SYMBOL;
+        else
+            token_type = TOKEN_TYPE.VAR;
         return {
-            val:    token,
-            type:   TOKEN_TYPE.SYMBOL
+            val: token,
+            type: token_type
         };
     }
 
     var interpreter = {
-        // implemented std lib as needed
-        STD_LIB: {
-            first: function(args){ return args[0]; }
+        REPL_STATE: newClojure(STD_LIB),
+        interpret: function (line) {
+            var tokens = tokenize(line);
+            var ast = parse(tokens);
+            return this.eval(ast);
         },
-        REPL_STATE: clojure({}, this.STD_LIB),
-        interpret: function(line){
-            var tokens = this.tokenize(line);
-            console.log(tokens);
-            var ast = this.ast(tokens);
-            console.log(ast);
-            var out = this.eval(ast);
-            console.log(out);
-            return line;
-        },
-        eval: function(ast){
-            var evalList = function(interpreter, ast, memory){
-                var evaluated = ast.map(function(atom){
+        eval: function (ast) {
+            var evalList = function (interpreter, ast, memory) {
+                var evaluated = ast.map(function (atom) {
                     var state = newClojure(memory);
-                    return evalHelper(interpreter, ast, state);
+                    return evalHelper(interpreter, atom, state);
                 });
-                if(isFunctionCall(evaluated))
+                if (isFunctionCall(evaluated)) {
                     return evaluated[0].val(evaluated.slice(1));
-                else
+                }
+                else {
                     return evaluated;
+                }
             };
-            var evalVar = function(interpreter, node, memory) {
+            var evalVar = function (interpreter, node, memory) {
+                // look up the variable
                 var variableMeaning = memory.get(node.val);
-                if(variableMeaning instanceof Function)
+                // create a clojure to a function
+                if (variableMeaning instanceof Function)
                     return {
                         type: TOKEN_TYPE.FUNC,
-                        val: function(args){ return memory.get(node.val).apply(null, args); }
+                        val: function (args) {
+                            return memory.get(node.val).apply(null, args);
+                        }
                     };
                 else
-                    return memory.get(node.val);
+                    return memory.get(node.val); // or just get the var value
             };
-            var evalHelper = function(interpreter, node, memory){
-                if(node instanceof Array)
+            var evalHelper = function (interpreter, node, memory) {
+                if (node instanceof Array) {
                     return evalList(interpreter, node, memory);
-                else if(node.type == TOKEN_TYPE.VAR)
+                }
+                else if (node.type == TOKEN_TYPE.VAR) {
                     return evalVar(interpreter, node, memory);
-                else
+                }
+                else if (node.type == TOKEN_TYPE.SYMBOL) {
                     return node.val;
+                }
+                else
+                    alert("WE FELL OUT OF THE BLOCK????? HOW????? " + node.type);
             };
             // combine the state of the repl with the std_lib
             var state = newClojure(this.REPL_STATE);
             return evalHelper(this, ast, state);
-        },
-
-        // a glorious regex to split things up
-        tokenize: function(line){
-            return  line
-                .replace(/\(/g, ' ( ' )
-                .replace(/\)/g, ' ) ' )
-                .split(/ +/);
-        },
-        // parse tokens into abstract syntax tree
-        ast: function(tokens){
-            // .. Mary Rose Cook.. we can change this later
-            astHelper = function(tokens, accum){
-                if(tokens.length == 0)
-                    return accum.pop();
-
-                var token = tokens.shift();
-                if(isStartOfExpression(token)){
-                    var childAst = astHelper(tokens, []);
-                    accum.push(childAst);
-                    return astHelper(tokens, accum);
-                }
-                else if(isEndOfExpression(token)){
-                    return accum;
-                }
-                else if(isWhiteSpace(token)){
-                    return astHelper(tokens, accum);
-                }
-                else {
-                    var node = identify(token);
-                    accum.push(node);
-                    return astHelper(tokens, accum);
-                }
-            };
-
-            return astHelper(tokens, []);
-        },
-        parse: function(line){
-            var tokens = this.tokenize(line);
         }
     };
 
@@ -128,12 +177,16 @@
     repl.disabled = true;
 
     var lisp = document.getElementById("lisp");
-    lisp.onkeypress = function(e){
-       if(!e) e = window.event;
-       var key = e.keyCode || e.which;
-       if(key == '13'){ // on enter
-           var new_line = lisp.value;
-           interpreter.interpret(new_line, repl);
-       }
+    lisp.onkeypress = function (e) {
+        if (!e) e = window.event;
+        var key = e.keyCode || e.which;
+        if (key == '13') { // on enter
+            var new_line = lisp.value;
+            var output = interpreter.interpret(new_line, repl);
+            console.log("interpreter output: " + output);
+
+            repl.value += "> " + new_line + "\n";
+            repl.value += output + "\n";
+        }
     };
 })();
